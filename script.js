579d6bfcd9045900790ad5aca582f001a64fee22
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-04-05 17:32:51 -0300

    feat: Added function that changes the style of the checkBox

--- script.js
+++ script.js
@@ -2,25 +2,38 @@ const form = document.querySelector('form')
 const input = document.querySelector('#newTask')
 const button = document.querySelector('button')
 const ul = document.querySelector('ul')
+// const li = document.querySelector('li')
+// const check = document.querySelector('#checkTask')
+
+
+const finishedTask = () => {
+  const check = document.querySelector('#checkTask')
+  const li = document.querySelector('li')
+  if (check.checked) {
+    li.classList.add('teste')
+    return
+  }
+  li.classList.remove('teste')
+}
+
+const addNewTask = (inputValue) => {
+  ul.innerHTML += `
+  <li>
+  <input type="checkbox" id="checkTask" onclick="finishedTask()">
+  ${inputValue}
+  <button>Delete</button>
+  </li>
+  `
+}
 
 const clearContent = () => {
   input.value = ''
 }
 
-const addNewTask = (inputValue) => {
-  ul.innerHTML += `
-    <li>
-    <input type="checkbox" id="checkTask">
-    ${inputValue}
-    <button>Delete</button>
-    </li>
-  `
-}
-
 form.addEventListener('submit', event => {
   event.preventDefault()
+  const check = document.querySelector('#checkTask')
   const inputValue = event.target.newTask.value
   addNewTask(inputValue)
-  
   clearContent()
 })
