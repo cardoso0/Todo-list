32bbdd92b9af5199042312f8d8069f2e398e81a4
Felipe Cardoso <felipepontescardoso@yahoo.com.br>
2022-04-06 09:27:24 -0300

    fix: fixed error that when adding new task reset the style of checkBox

--- script.js
+++ script.js
@@ -5,27 +5,49 @@ const ul = document.querySelector('ul')
 
 const finishedTask = (clickedElement) => {
   const checkDataValue = clickedElement.dataset.check
+  const todo = document.querySelector(`[data-todo="${checkDataValue}"]`)
   const checkBox = document.querySelector(`[data-check="${checkDataValue}"]`)
   const span = document.querySelector(`[data-item="${checkDataValue}"]`)
-  
+  console.log(todo)
+  console.log(checkBox.checked)
   if (checkBox.checked) {
-    span.classList.add('finished')
+    todo.classList.add('finished')
     return
   }
-  span.classList.remove('finished')
+  todo.classList.remove('finished')
+}
+
+const createItemList = (inputValue) => {
+  let li = document.createElement('li')
+  let checkBox = document.createElement('input')
+  let span = document.createElement('span')
+  let btn = document.createElement('button')
+  ul.appendChild(li)
+  li.appendChild(checkBox)
+  li.appendChild(span)
+  li.appendChild(btn)
+  li.setAttribute('data-todo', `${inputValue}`)
+  checkBox.setAttribute('type', 'checkbox')
+  checkBox.setAttribute('id', 'checkTask')
+  checkBox.setAttribute('data-check', `${inputValue}`)
+  span.textContent = inputValue
+  span.setAttribute('data-item', `${inputValue}`)
+  btn.textContent = '🗑️'
+  btn.setAttribute('data-trash', `${inputValue}`)
 }
 
 const addNewTask = (inputValue) => {
-  if (inputValue.length) {
-    ul.innerHTML += `
-    <li data-todo="${inputValue}">
-      <input type="checkbox" id="checkTask" data-check="${inputValue}">
-      <span data-item="${inputValue}">${inputValue}</span>
-      <button data-trash="${inputValue}">🗑️</button>
-    </li>
-    `
-  }
-
+  // if (inputValue.length) {
+  //   ul.innerHTML += `
+  //   <li data-todo="${inputValue}">
+  //     <input type="checkbox" id="checkTask" data-check="${inputValue}">
+  //     <span data-item="${inputValue}">${inputValue}</span>
+  //     <button data-trash="${inputValue}">🗑️</button>
+  //   </li>
+  //   `
+  // }
+  if (inputValue.length) createItemList(inputValue)
+  
   event.target.reset()
 }
 
